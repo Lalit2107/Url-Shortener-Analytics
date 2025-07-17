42acb84710675fe8eed0af52e371aa97237d69b3
Lalit2107 <157688318+Lalit2107@users.noreply.github.com>
2025-07-17 12:28:26 +0530

    Delete url-shortener-frontend directory

--- url-shortener-frontend/src/utils/constant.js
+++
@@ -1,6 +0,0 @@
-import AppRouter, { subDomainRouter } from "../AppRouter";
-
-export const subDomainList = [
-    {subdomain:"www", app: AppRouter, main: true},
-    {subdomain:"url", app: subDomainRouter, main: false}
-];

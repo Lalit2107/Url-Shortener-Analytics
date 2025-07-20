1626d321b64e757bc2c2c51f47aa25747260a785
Lalit2107 <157688318+Lalit2107@users.noreply.github.com>
2025-07-20 10:30:25 +0530

    initial commit for frontend

---
+++ url-shortener-frontend/src/contextapi/ContextApi.jsx
@@ -0,0 +1,28 @@
+import { useContext } from "react";
+import { createContext, useState } from "react";
+
+const ContextApi = createContext();
+
+export const ContextProvider = ({ children }) => {
+  const getToken = localStorage.getItem("JWT_TOKEN")
+    ? JSON.parse(localStorage.getItem("JWT_TOKEN"))
+    : null;
+
+  const [token, setToken] = useState(getToken);
+
+  const sendData = {
+    token,
+    setToken,
+  };
+
+  return (
+    <ContextApi.Provider value={sendData}>
+      {children}
+    </ContextApi.Provider>
+  );
+};
+
+export const useStoreContext = () =>{
+   const context = useContext(ContextApi);
+   return context;
+}
